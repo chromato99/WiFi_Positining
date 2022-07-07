8fa49c90e0b13a2fea400f7834563ac19f970af3
chromato99 <chromato99@gmail.com>
2022-07-08 00:48:24 +0900

    refactoring

--- app/src/main/java/com/example/wifi_positining/WifiScanActivity.java
+++ app/src/main/java/com/example/wifi_positining/WifiScanActivity.java
@@ -34,7 +34,7 @@ public class WifiScanActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wifi_scan);
+        setContentView(R.layout.activity_wifi_rtt_test);
 
         Button button = findViewById(R.id.button);
 
